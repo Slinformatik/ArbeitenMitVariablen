bcfa3919c5edd4698e35f0fcd5502c4095f96918
Slinformatik <nilsdiercks@outlook.de>
2021-10-21 11:23:41 +0200

    Projektdateien hinzufügen.

---
+++ ArbeitenMitVariablen/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace ArbeitenMitVariablen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string namenils = "Nils";
+            string nameSven = "Sven";
+            string begruessung = "Guten Tag";
+            string abschied = "Schönen Tag";
+            string begruessungVonNils = begruessung+" "+namenils;
+            string verabschiedeNils = abschied+" "+namenils;
+            string begruessungVonSven = begruessung +    " "    +nameSven;
+            string verabschiedungVonSven = abschied + nameSven;
+            Console.WriteLine(begruessungVonNils);
+            Console.WriteLine(verabschiedeNils);
+            Console.WriteLine(begruessungVonSven);
+            Console.WriteLine(verabschiedungVonSven);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+        
+    }
+    
+}
